a1bb0f5d9343c6825e82d9d19fed4969669cbbe4
Harrison Nguyen <53590998+harrison112@users.noreply.github.com>
2022-08-29 18:42:18 +1000

    Installed packages and plugins
    AR Foundation package installed from Package Manager (Window -> Package Manager) and installed XR Plug-in management with the ARCore ticked.
    
    Brought in some new experimental phone camera scripts in attempt to get the camera working for ARPageScene (work in progress)

---
+++ NaviAR_0.1/Assets/Scripts/phoneCameraScript2.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class phoneCameraScript2 : MonoBehaviour
+{
+
+    int currentCamIndex = 0;
+
+    WebCamTexture webCamTexture;
+    public string path;
+    public RawImage display;
+
+
+    public void Start()
+    {
+        webCamTexture = new WebCamTexture();
+        GetComponent<Renderer>().material.mainTexture = webCamTexture;
+        webCamTexture.Play(); //Start Playing
+    }
+
+
+}
